65308eb4d5ea189b5d38472b5e605efd2e54b91a
Adam Glumbik <adam.glumbik@mhdirekt.com>
2024-08-04 12:00:33 +0200

    Kraaaa

--- screens/Scaner.js
+++ screens/Scaner.js
@@ -3,7 +3,7 @@ import { View, Text } from "react-native"
 export default function ScanerScreen() {
     return (
         <View style={{flex:1, alignItems: 'center', justifyContent: 'center'}}>
-            <Text>ScanerScreen</Text>
+            <Text>ScanerScreen kraaaaas</Text>
         </View>
     )
 }
