e4bb4c280f2bda4ccb48e79b62b347c8c3b3a51b
Adam Glumbik <adam.glumbik@mhdirekt.com>
2024-08-08 09:40:24 +0200

    BarCodeScanner FIX

--- app/screens/ScannerCode.js
+++ app/screens/ScannerCode.js
@@ -1,37 +1,51 @@
-import React, {useEffect, useRef} from 'react';
-import { View,Text,StyleSheet } from 'react-native';
+import React, {useState, useEffect, useRef} from 'react';
+import { View,Text,StyleSheet,LogBox,Button } from 'react-native';
 import { ActivityIndicator } from 'react-native-paper';
-import { Camera, useCameraPermission,useCameraDevice,useCodeScanner } from 'react-native-vision-camera';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+LogBox.ignoreLogs(['Warning: ...']);
+LogBox.ignoreAllLogs();
 
 export default function ScannerCodeScreen() {
-    const { hasPermission, requestPermission } = useCameraPermission()
-    const device = useCameraDevice('back')
-    
-    const codeScanner = useCodeScanner({
-      codeTypes: ['qr', 'ean-13'],
-      onCodeScanned: (codes) => {
-        console.log(`Scanned ${codes.length} codes!`)
-      }
-    })
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
 
-    useEffect(() => {
-      if(!hasPermission) {
-        requestPermission();
-      }
-    }, [hasPermission])
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    };
 
-    if(!hasPermission) {
-      return <ActivityIndicator />
-    }
+    getBarCodeScannerPermissions();
+  }, []);
 
-    if(!device) {
-      return <Text>Camera device not found</Text>
-    }
-  
-    return (
-      <View style={{flex:1}}>
-        <Camera codeScanner={codeScanner} style={StyleSheet.absoluteFill} device={device} isActive={true} />
-      </View>    
-    )
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
 
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+});
